a557980d74b31b0f71ca5431214005060da678a6
GrizzlyMain <ryan.thom.dalton@gmail.com>
2025-02-12 15:42:04 -0600

    Initial commit

---
+++ src/main/java/com/dingers/dingers_spring/DingersSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dingers.dingers_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingersSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DingersSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dingers/dingers_spring/DingersSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingers.dingers_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingersSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
